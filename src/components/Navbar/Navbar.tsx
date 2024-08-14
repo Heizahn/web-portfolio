2308bbcbb45a155aa7c7eb678642c858ad63e6f3
Humberto <107310957+Heizahn@users.noreply.github.com>
2024-08-14 14:23:00 -0400

    Actualizar Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
 		inView('#projects', (e) => {
 			let positionY = e.boundingClientRect.y;
 
-			if (positionY < 40.0) {
+			if (positionY < 45.0) {
 				setInViewStudy(false);
 				setInViewProjects(true);
 				setInViewAbout(false);
@@ -42,7 +42,7 @@ export default function Navbar() {
 
 		inView('#about', (e) => {
 			let positionY = e.boundingClientRect.y;
-			if (positionY < 45.0) {
+			if (positionY < 40.0) {
 				setInViewStudy(false);
 				setInViewProjects(false);
 				setInViewAbout(true);
